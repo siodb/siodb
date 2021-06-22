9b9e7d84607daf21a456d2b3e4138d2cfe862514
Nicolas <nicolas@siodb.io>
2021-06-22 06:52:58 +0000

    fix header

--- tests/scenarios/sql/query_test_data_model.sql
+++ tests/scenarios/sql/query_test_data_model.sql
@@ -1,3 +1,6 @@
+-- Copyright (C) 2019-2020 Siodb GmbH. All rights reserved.
+-- Use of this source code is governed by a license that can be found
+-- in the LICENSE file.
 
 drop database
 if exists db1;
