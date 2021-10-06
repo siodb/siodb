93b1dc328c95fd87174af631865bf8845d30cd37
Nicolas Penot <nicolas@siodb.io>
2021-10-06 11:31:12 +0200

    impr(rest_server): Set Access-Control-Allow #190 (#194)

--- rest_server/RestWorker.go
+++ rest_server/RestWorker.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2020 Siodb GmbH. All rights reserved.
+// Copyright (C) 2019-2021 Siodb GmbH. All rights reserved.
 // Use of this source code is governed by a license that can be found
 // in the LICENSE file.
 
@@ -16,9 +16,21 @@ type restWorker struct {
 	Port      uint32
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE")
+
+	}
+}
+
 func (worker *restWorker) CreateRouter(Port uint32) (err error) {
 	worker.Port = Port
 	worker.ginEngine = gin.New()
+	worker.ginEngine.Use(CORSMiddleware())
 
 	// GET
 	worker.ginEngine.GET("/databases", worker.getDatabases)
